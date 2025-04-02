484bf458ce064ffd6d876729445de358d9e4aac6
Mark Muetzelfeldt <markmuetz@gmail.com>
2025-04-02 21:29:37 +0100

    New slurm parsing script to give list of all active slurm jobs for jobids.

---
+++ scripts/convert_latlon_pp_to_hp_nc/parse_sacct_output.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+import sys
+import json
+from pathlib import Path
+import subprocess as sp
+
+import pandas as pd
+
+
+jobids_path = Path(sys.argv[1])
+with jobids_path.open('r') as f:
+    jobids = json.load(f)
+
+jobids_str = ','.join(str(j) for j in jobids)
+
+cmd = f"sacct -P -o 'jobid%20,elapsed,state,MaxRSS,NodeList' -j {jobids_str}|grep -E '^[0-9_]*\.batch\|' --color=Never"
+# print(cmd)
+
+result = sp.run(cmd, capture_output=True, text=True, shell=True)
+# print(result.stdout)
+lines = [l for l in result.stdout.split('\n') if l]
+data = [line.split('|') for line in lines]
+
+df = pd.DataFrame(data, columns=["jobid", "elapsed", "state", "maxrss", "host"])
+df = df[df.state != 'FAILED']
+df['maxrss'] = df['maxrss'].str.rstrip('K').replace('', '0').astype(float) / 1e6
+print(df.to_string())
+
+df["elapsed"] = pd.to_timedelta(df["elapsed"])
+
+# df.state == 'COMPLETED'
+# df[df.state == 'COMPLETED'].elapsed
+
+print('mean:', df[df.state == 'COMPLETED'][['elapsed', 'maxrss']].mean())
+print('min:', df[df.state == 'COMPLETED'][['elapsed', 'maxrss']].min())
+print('max:', df[df.state == 'COMPLETED'][['elapsed', 'maxrss']].max())
