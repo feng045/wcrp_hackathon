40a7a91b4c0e6a6fd33ad325f78e579fce94c594
Mark Muetzelfeldt <markmuetz@gmail.com>
2025-04-14 22:11:13 +0100

    Debug coarsening for 3d data.

--- scripts/convert_latlon_pp_to_hp_nc/healpix_coarsen.py
+++ scripts/convert_latlon_pp_to_hp_nc/healpix_coarsen.py
@@ -7,21 +7,26 @@ import xarray as xr
 from dask.diagnostics import ProgressBar
 from loguru import logger
 
-from processing_config import chunks2d
+from processing_config import processing_config
 
 
 def nan_weight(arr, axis):
     return ((~np.isnan(arr)).sum(axis=axis) / 4).astype(np.float32)
 
 
-def map_regional_to_global(_da, src_zoom):
+def map_regional_to_global(_da, src_zoom, dim):
     logger.debug(_da.name)
     coords = {
         n: c for n, c in _da.coords.items()
         if n != 'cell'
     }
     coords['cell'] = np.arange(12 * 4 ** src_zoom)
-    dims = [d for d in coords if d != 'crs']
+
+    if dim == '2d':
+        dims = ['time', 'cell']
+    elif dim == '3d':
+        dims = ['time', 'pressure', 'cell']
+    # dims = [d for d in coords if d != 'crs']
     shape = list(_da.shape[:-1]) + [len(coords['cell'])]
     # Would be nice, but it's actually quite a bit slower?
     # dummies = dask.array.zeros(shape, dtype=np.float32)
@@ -35,14 +40,17 @@ def map_regional_to_global(_da, src_zoom):
     return da_new
 
 
-def map_global_to_regional(_da, src_ds_region, tgt_ds_store):
+def map_global_to_regional(_da, src_ds_region, tgt_ds_store, dim):
     logger.debug(_da.name)
     coords = {
         n: c for n, c in _da.coords.items()
         if n != 'cell'
     }
     coords['cell'] = tgt_ds_store.cell
-    dims = [d for d in coords if d != 'crs']
+    if dim == '2d':
+        dims = ['time', 'cell']
+    elif dim == '3d':
+        dims = ['time', 'pressure', 'cell']
     da = list(src_ds_region.data_vars.values())[0]
     shape = list(da.shape[:-1]) + [len(coords['cell'])]
 
@@ -51,15 +59,17 @@ def map_global_to_regional(_da, src_ds_region, tgt_ds_store):
     return da_new
 
 
-def calc_tgt_weights(src_ds, src_ds_region, tgt_ds, tgt_zoom):
+def calc_tgt_weights(src_ds, src_ds_region, tgt_ds, tgt_zoom, dim):
     if 'weights' in src_ds_region:
         # TODO: Need to handle lower zooms when weights already exist.
         src_weights = src_ds_region['weights']
         weights = src_weights.coarsen(cell=4).mean()
     else:
-        # breakpoint()
         # It is enough to calc the weights for one field, and store.
-        src_da = list(src_ds.data_vars.values())[0].isel(time=0)
+        if dim == '2d':
+            src_da = list(src_ds.data_vars.values())[0].isel(time=0)
+        elif dim == '3d':
+            src_da = list(src_ds.data_vars.values())[0].isel(time=0, pressure=0)
         weights = src_da.coarsen(cell=4).reduce(nan_weight).compute()
     cells = np.arange(12 * 4 ** tgt_zoom)
     coords = {'cell': cells}
@@ -83,11 +93,12 @@ def coarsen_healpix_zarr_region(src_ds, tgt_store, tgt_zoom, dim, start_idx, end
 
     src_ds_region = src_ds.isel(time=time_slice)
     if regional:
-        src_ds_region = src_ds_region.map(map_regional_to_global, src_zoom=src_zoom)
+        src_ds_region = src_ds_region.map(map_regional_to_global, src_zoom=src_zoom, dim=dim)
 
     # To compute or not to compute...
     # Computing would force the download and loading into mem, but might stop contention for the resource?
     # tgt_ds = src_ds_region.coarsen(cell=4).mean()
+    logger.debug('compute tgt_ds')
     tgt_ds = src_ds_region.coarsen(cell=4).mean().compute()
 
     logger.info('modify metadata')
@@ -103,9 +114,9 @@ def coarsen_healpix_zarr_region(src_ds, tgt_store, tgt_zoom, dim, start_idx, end
 
     if regional:
         tgt_ds_store = xr.open_zarr(tgt_store, chunks=zarr_chunks)
-        tgt_ds = tgt_ds.map(map_global_to_regional, src_ds_region=src_ds_region, tgt_ds_store=tgt_ds_store)
+        tgt_ds = tgt_ds.map(map_global_to_regional, src_ds_region=src_ds_region, tgt_ds_store=tgt_ds_store, dim=dim)
         if src_ds_region.time[0] == src_ds.time[0]:
-            tgt_weights = calc_tgt_weights(src_ds, src_ds_region, tgt_ds, tgt_zoom)
+            tgt_weights = calc_tgt_weights(src_ds, src_ds_region, tgt_ds, tgt_zoom, dim)
             # TODO: Need to create weights for this to work.
             # tgt_ds['weights'] = tgt_weights
             # logger.debug(np.isnan(tgt_ds.weights).sum())
@@ -130,14 +141,22 @@ def coarsen_healpix_zarr_region(src_ds, tgt_store, tgt_zoom, dim, start_idx, end
 if __name__ == '__main__':
     s3cfg = dict([l.split(' = ') for l in Path('/home/users/mmuetz/.s3cfg').read_text().split('\n') if l])
     sim = sys.argv[1]
-    freq = sys.argv[2]
+    dim = sys.argv[2]
     zoom = int(sys.argv[3])
     start_idx = int(sys.argv[4])
     end_idx = int(sys.argv[5])
+    freqs = {
+        '2d': 'PT1H',
+        '3d': 'PT3H',
+    }
+    freq = freqs[dim]
     urls = {
         z: 'sim-data/dev/{sim}/v4/data.healpix.{freq}.z{zoom}.zarr'.format(sim=sim, freq=freq, zoom=z)
         for z in range(11)
     }
+
+    config = processing_config[sim]
+    chunks = config['groups'][dim]['chunks']
 
     jasmin_s3 = s3fs.S3FileSystem(
         anon=False, secret=s3cfg['secret_key'],
@@ -147,7 +166,7 @@ if __name__ == '__main__':
     src_store = s3fs.S3Map(root=urls[zoom], s3=jasmin_s3, check=False)
     test_tgt_store = s3fs.S3Map(root=urls[zoom - 1], s3=jasmin_s3, check=False)
 
-    zarr_chunks = {'time': chunks2d[zoom - 1][0], 'cell': -1}
+    zarr_chunks = {'time': chunks[zoom - 1][0], 'cell': -1}
     test_src_ds = xr.open_zarr(src_store, chunks=zarr_chunks)
 
     # tgt_calcs = find_tgt_calcs(urls, chunks=chunks2d, dim='2d', variable='tas')
@@ -155,5 +174,5 @@ if __name__ == '__main__':
     # start_idx = tgt_time_calcs[zoom - 1][0]['start_idx']
     # end_idx = tgt_time_calcs[zoom - 1][0]['end_idx']
     logger.debug((start_idx, end_idx))
-    coarsen_healpix_zarr_region(test_src_ds, test_tgt_store, zoom - 1, '2d', start_idx, end_idx, chunks2d,
-                                regional=True)
+    coarsen_healpix_zarr_region(test_src_ds, test_tgt_store, zoom - 1, dim, start_idx, end_idx, chunks,
+                                regional=config['regional'])
