4c0ae8089238859566137e57d9c6166e63cb4f45
Mark Muetzelfeldt <markmuetz@gmail.com>
2025-05-03 16:56:25 +0100

    Rewrite to make a bit more fault-tolerant.

--- scripts/process_um_data/calc_completed_chunks.py
+++ scripts/process_um_data/calc_completed_chunks.py
@@ -1,7 +1,10 @@
+import asyncio
 import sys
 import math
+import random
 from itertools import groupby
 from pathlib import Path
+from time import sleep
 
 from loguru import logger
 import numpy as np
@@ -12,17 +15,18 @@ from processing_config import processing_config
 
 s3cfg = dict([l.split(' = ') for l in Path('/home/users/mmuetz/.s3cfg').read_text().split('\n') if l])
 
-jasmin_s3 = s3fs.S3FileSystem(
-    anon=False, secret=s3cfg['secret_key'],
-    key=s3cfg['access_key'],
-    # client_kwargs={'endpoint_url': 'https://hackathon-o.s3-ext.jc.rl.ac.uk'}
-    client_kwargs={'endpoint_url': 'http://hackathon-o.s3.jc.rl.ac.uk'}
-)
-
 def get_initialized_chunk_ids(url, variable):
     """Find all initalized chunks for variable.
 
     does an `ls` on the path, and perses the output."""
+    # I think I might need to recreate this obj every time, or I get a:
+    # PermissionError: Invalid token, possibly expired
+    jasmin_s3 = s3fs.S3FileSystem(
+        anon=False, secret=s3cfg['secret_key'],
+        key=s3cfg['access_key'],
+        client_kwargs={'endpoint_url': 'http://hackathon-o.s3.jc.rl.ac.uk'}
+    )
+
     lsout = jasmin_s3.ls(f'{url}/{variable}/')
     fnames = [p.split('/')[-1] for p in lsout]
     chunk_ids_str = [p for p in fnames if not p.startswith('.z')]
@@ -54,27 +58,53 @@ def find_completed_time_chunks(src_ds, chunksize, complete_size, zoom, urls, var
 def find_tgt_can_complete(factor, src_completed, tgt_completed):
     """Based on factor, see which tgt chunks can be completed from completed src chunks."""
     tgt_can_complete = np.zeros_like(tgt_completed, dtype=bool)
+    # Length of complete chunks. Final possibly shorter chunk handled below.
     src_n = len(src_completed) - len(src_completed) % factor
     tgt_n = src_n // factor
     tgt_can_complete[:tgt_n] = src_completed[:src_n].reshape(-1, factor).all(axis=-1)
+    # Handle shorter chunk.
     tgt_can_complete[-1] = len(src_completed[src_n:]) != 0 and src_completed[src_n:].all()
     return tgt_can_complete
 
 
-def find_tgt_calcs(urls, chunks, variable='ta', max_zoom=10, dim='2d'):
+async def async_retry_open_zarr(url, max_retries=10):
+    retries = 0
+    while retries < max_retries:
+        try:
+            ds = xr.open_zarr(url)
+            logger.debug(f'Successfully opened {url}')
+            return ds
+        except Exception as e:
+            # This has started (30/4/2025) raising exceptions
+            # It has previously been fine.
+            logger.warning(f'Failed to open {url}')
+            logger.warning(e)
+            retries += 1
+            # Sleep 10s, then 20s... with 5s jitter.
+            timeout = 10 * retries + random.uniform(-5, 5)
+            logger.warning(f'sleeping for {timeout} s')
+            await asyncio.sleep(timeout)
+    raise Exception(f'failed to open {url} after {retries} retries')
+
+
+def find_tgt_calcs(urls, chunks, variable='ta', max_zoom=10, dim='2d', end_time='2021-03-01 00:00'):
     """Loop over zoom, starting at highest, working out which chunks can be completed and need calculating
 
     src: higher zoom level
     tgt: lower zoom level"""
     tgt_calcs = {}
+    tgt_ds = None
     for src_zoom in range(max_zoom, 0, -1):
         tgt_zoom = src_zoom - 1
         logger.info(f'Calculating jobs to do for z{tgt_zoom} {urls[tgt_zoom]}')
         if src_zoom == max_zoom:
-            src_ds = xr.open_zarr('http://hackathon-o.s3.jc.rl.ac.uk/' + urls[src_zoom])
+            src_ds = asyncio.run(async_retry_open_zarr('http://hackathon-o.s3.jc.rl.ac.uk/' + urls[src_zoom]))
         else:
             src_ds = tgt_ds
-        tgt_ds = xr.open_zarr('http://hackathon-o.s3.jc.rl.ac.uk/' + urls[tgt_zoom])
+        tgt_ds = asyncio.run(async_retry_open_zarr('http://hackathon-o.s3.jc.rl.ac.uk/' + urls[tgt_zoom]))
+        if end_time:
+            src_ds = src_ds.sel(time=slice(end_time))
+            tgt_ds = tgt_ds.sel(time=slice(end_time))
         src_ncell = len(src_ds.cell)
         tgt_ncell = len(tgt_ds.cell)
 
@@ -87,6 +117,8 @@ def find_tgt_calcs(urls, chunks, variable='ta', max_zoom=10, dim='2d'):
             npressure = 25
             src_complete_size = npressure / src_chunksize[1] * src_ncell / src_chunksize[2]
             tgt_complete_size = npressure / tgt_chunksize[1] * tgt_ncell / tgt_chunksize[2]
+        else:
+            raise Exception(f'unknown dim {dim}')
 
         assert tgt_chunksize[0] % src_chunksize [0] == 0, 'target chunksize 0 does not exactly divide source'
         factor = tgt_chunksize[0] // src_chunksize[0]
@@ -97,15 +129,17 @@ def find_tgt_calcs(urls, chunks, variable='ta', max_zoom=10, dim='2d'):
             src_completed = tgt_will_be_complete
 
         tgt_completed = find_completed_time_chunks(tgt_ds, tgt_chunksize, tgt_complete_size, tgt_zoom, urls, variable)
-        logger.debug(f'already completed {tgt_completed.sum()}')
+        logger.debug(f'already completed: {tgt_completed.sum()}, remaining: {(~tgt_completed).sum()}')
         tgt_can_complete = find_tgt_can_complete(factor, src_completed, tgt_completed)
-        logger.debug(f'can complete by coarsening {tgt_can_complete.sum()}')
+        logger.debug(f'can complete by coarsening: {tgt_can_complete.sum()}, cannot complete: {(~tgt_can_complete).sum()}')
+        logger.trace(f'cannot complete idx: {np.where(~tgt_can_complete)}')
 
         tgt_calc = tgt_can_complete & ~tgt_completed
-        logger.debug(f'adding {tgt_calc.sum()} new calcs')
+        logger.debug(f'adding: {tgt_calc.sum()} new calcs')
         tgt_will_be_complete = tgt_completed | tgt_can_complete
-        logger.debug(f'will be complete {tgt_will_be_complete.sum()}')
+        logger.debug(f'will be complete: {tgt_will_be_complete.sum()}, remaining: {(~tgt_will_be_complete).sum()}')
         if not tgt_will_be_complete.any():
+            logger.debug(f'No target chunks are completable at {tgt_zoom}')
             break
         tgt_calcs[tgt_zoom] = tgt_calc
     return tgt_calcs
